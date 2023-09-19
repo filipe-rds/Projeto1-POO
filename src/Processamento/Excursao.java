1466a6fab3d3a44e40219eeb09ebf4e600e3876c
Gabriel Félix <gfedacs@hotmail.com>
2023-09-19 12:32:36 -0300

    commit-gabriel
    commit-gabriel

--- src/Processamento/Excursao.java
+++ src/Processamento/Excursao.java
@@ -54,7 +54,9 @@ public class Excursao {
 	
 	public void criarReserva(String cpf,String nome)throws Exception {
 	// Adiciona uma reserva “cpf/nome”
-	// Verificar se existe um nome igual na lista
+		
+	// Verificar se existe um nome igual na lista 
+		
 		if(listaReserva.size()< maxReservas) {
 			for (int i = 0; i < listaReserva.size(); i++ ) {
 				String temp = listaReserva.get(i);
@@ -68,37 +70,69 @@ public class Excursao {
 			
 		}
 		else{
-			throw new Exception("Lista de Reserva está preenchida");
+			throw new Exception("Limite excedido");
 		}
 	}
-//	public void cancelarReserva(String cpf, String nome) {
-//	// Remove uma reserva “cpf/nome”
-//	
-//	}
-//	
-//	public void cancelarReserva(String cpf) {
-//	// Remove todas as reservas do cpf
-//	}
-//	
-//	
-//	
-//	public double calcularValorTotal() {
-//	// Calcular valor total da excursão = preço * qde de reservas
-//		
-//	}
+	// Methods
+
+	public void cancelarReserva(String cpf, String nome) throws Exception {
+	    int codcpf = 0;
+	    int codnome = 0;
+	    int codconfirmacao = 0;
+	    if (listaReserva.size() > 0) {
+	        for (int i = 0; i < listaReserva.size(); i++) {
+	            String temp = listaReserva.get(i);
+	            String[] separacao = temp.split("/");
+	            codnome = 0;
+	            codcpf = 0;
+	            
+	            if (separacao[0].equals(cpf)) {
+	                codconfirmacao = 5; // Existe o CPF OK
+	                codcpf = 0;
+	            }if(!separacao[0].equals(cpf)) {
+	                codcpf = 5; // Não existe o cpf
+	            }
+
+	            if (!separacao[1].equals(nome)) {
+	                
+	            }
+	            if (separacao[1].equals(nome) || codconfirmacao == 5) {
+	                listaReserva.remove(i);
+	            }
+	        }
+	        if (codcpf == 5 && codnome == 10) {
+	            throw new Exception("Não existe esse cpf, e esse nome");
+	        }
+	        if (codcpf == 5 && codnome == 0) {
+	            throw new Exception("Não existe esse cpf");
+	        }
+	        if (codcpf == 0 && codnome == 10) {
+	        	
+	            throw new Exception("Não existe esse nome");
+	        }
+	    }else
+			throw new Exception("Não é possível remover, pois a lista está vazia");
+	}
 	
+	public void cancelarReserva(String cpf) throws Exception {
+	    int i = 0;
+	    while (i < listaReserva.size()) {
+	        String temp = listaReserva.get(i);
+	        String[] separacao = temp.split("/");
+	        if (separacao[0].equals(cpf)) {
+	            listaReserva.remove(i);
+	            System.out.println(listaReserva.size());
+	        } else {
+	            i++; 
+	        }
+	        if (listaReserva.size()<=0) {
+	        	throw new Exception("Não é possível remover, pois a lista está vaziaaa");
+	        }
+		
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+
+		
+	}}
+		
+		
--- src/Processamento/Main.java
+++ src/Processamento/Main.java
@@ -4,15 +4,54 @@ public class Main {
 
 	public static void main(String[] args)  {
 		
-		Excursao teste = new Excursao(1337,18000.0, 7);
+		Excursao teste = new Excursao(1337,18000.0, 10);
 		try {
-			teste.criarReserva("123", "Filipin do Grau");
-			teste.criarReserva("1898", "Filipin do Grau");
+			teste.criarReserva("123","Felipe");
+			teste.criarReserva("123","Felip");
+			teste.criarReserva("123","Feli");
+			;
 		}catch(Exception f){
+			
 			System.out.println("Erro ao criar reserva: " + f.getMessage());
 		}
 		
-
+		try {
+			teste.cancelarReserva("123");
+		}
+		
+		catch(Exception e) {
+			System.out.println("Erro cancelar reserva: "+e.getMessage());
+		}
+		
+		try {
+			teste.criarReserva("123","Felipe");
+			teste.criarReserva("123","Felip");
+			teste.criarReserva("123","Feli");
+			
+			;
+		}catch(Exception f){
+			
+			System.out.println("Erro ao criar reserva: " + f.getMessage());
+		}
+		
+		try {
+			teste.cancelarReserva("123");
+		}
+		
+		catch(Exception e) {
+			System.out.println("Erro cancelar reserva: "+e.getMessage());
+		}
+		
+		try {
+			teste.cancelarReserva("123");
+		}
+		
+		catch(Exception e) {
+			System.out.println("Erro cancelar reserva: "+e.getMessage());
+		}
+		
 	}
+	
+	
 
 }
